448630e9cd357d0d81e761293961f86f0efbf98b
tylercallison <tcsoccer2000@gmail.com>
2022-07-13 23:10:56 -0700

    feat: added individual pages

--- components/nav.tsx
+++ components/nav.tsx
@@ -87,7 +87,6 @@ const Nav = () => {
           id="mobile-menu"
         >
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-            {router.asPath.toString()}
             <NavLink title="Home" href="/" route={route} />
             <NavLink title="About" href="/about" route={route} />
             <NavLink title="Works" href="/works" route={route} />
