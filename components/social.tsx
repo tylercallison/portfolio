f930b20bc2124237b2d7f618122b7e7facc90c1e
tylercallison <tcsoccer2000@gmail.com>
2022-07-14 15:59:52 -0700

    fix: social buttons tighter to logo

--- components/social.tsx
+++ components/social.tsx
@@ -2,18 +2,25 @@ import Image from "next/future/image";
 
 const Social = () => {
   return (
-    <div className="w-24 mt-6">
-      <div className="grid grid-cols-2 items-center">
+    <div className="w-20 mt-6">
+      <div className="grid grid-cols-2 items-center gap-6">
         <a href="https://www.linkedin.com/in/tyler-callison/">
           <Image
             src="/logos/linkedin.svg"
             alt="Linkedin"
             width={25}
             height={25}
+            className="w-full"
           />
         </a>
         <a href="https://github.com/tylercallison">
-          <Image src="/logos/github.svg" alt="Github" width={25} height={25} />
+          <Image
+            src="/logos/github.svg"
+            alt="Github"
+            width={25}
+            height={25}
+            className="w-full"
+          />
         </a>
       </div>
     </div>
